a52a14abe44a6f78ca08d432e9c3b3ecbd1a97fa
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-25 16:07:01 -0400

    update layout with search bar

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -4,17 +4,27 @@ import matter from "gray-matter";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
+import CategoryList from "@/components/CategoryList";
 import { getPosts } from "@/lib/posts";
 
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">Posts in {categoryName}</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5 font-bold">
+            Posts in {categoryName}
+          </h1>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
 
     </Layout>
@@ -50,12 +60,17 @@ export async function getStaticProps({params:{category_name}}) {
 
 const posts = getPosts()
 
+  const categories = posts.map((post) => post.frontmatter.category);
+
+  const uniqueCategories = [...new Set(categories)];
+
 const categoryPosts = posts.filter( post=> post.frontmatter.category.toLowerCase() === category_name )
 
   return {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories
     },
   };
 }
