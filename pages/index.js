32729382aa61e6cba6ac6db07b040f4a580542b5
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-05-17 17:09:10 -0400

    update spelling error

--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,24 @@ import { getPosts } from '@/lib/posts'
 export default function HomePage( {posts} ) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold' >Recomended Books</h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5' >
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Recommended Books</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
       </div>
-      <Link legacyBehavior href='/blog' >
-<a className='block text-center border
+      <Link legacyBehavior href="/blog">
+        <a
+          className="block text-center border
 border-gray-500 text-gray-800 rounded-md py-4 my-5
 transition duration-500 ease select-none
 hover:text-white hover:bg-gray-900
-focus:outline-none focus:shadow-outline w-full'>
-All books
-</a>
+focus:outline-none focus:shadow-outline w-full">
+          All books
+        </a>
       </Link>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
